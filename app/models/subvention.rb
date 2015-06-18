0029fba51439a09e378b303010551f3db410b828
Tanguy PATTE <tanguy.patte@gmail.com>
2015-06-18 15:37:01 +0200

    add done and not done filter

--- app/models/subvention.rb
+++ app/models/subvention.rb
@@ -1,5 +1,14 @@
 class Subvention < ActiveRecord::Base
+
   def self.find_by_siret(siret)
     where(NumSiret: siret)
+  end
+
+  def self.done(siret)
+    find_by_siret(siret).where(IndicSoldeDossier: 'O').where(IndicPayeDossier: 'O')
+  end
+
+  def self.not_done(siret)
+    find_by_siret(siret).where.not(IndicSoldeDossier: 'O').where.not(IndicPayeDossier: !'O')
   end
 end

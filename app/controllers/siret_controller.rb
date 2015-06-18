35bb4d6794798826b564d58c5110c34eeabf142e
Tanguy PATTE <tanguy.patte@gmail.com>
2015-06-18 15:01:27 +0200

    add update template

--- app/controllers/siret_controller.rb
+++ app/controllers/siret_controller.rb
@@ -4,6 +4,10 @@ class SiretController < ApplicationController
   def update
     puts "============"
     puts params
+    @subventions = Subvention.find_by_siret(params[:siret])
+    sub = @subventions.first
+    puts sub.RSocMo
+    @entreprise =  Hashie::Mash.new({siret: sub.NumSiret, raison_social: sub.RSocMo, l1: sub.RueMo.gsub('*',' ').gsub(/-.*/,'') , l2: sub.VilleResidMo, l3: sub.CodePostalMo})
 
   end
 end
